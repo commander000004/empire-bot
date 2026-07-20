f2c8048b4247c6be15ef5929508c960312855f27
commander000004 <commander04.1391t@gmail.com>
2026-07-20 18:01:27 +0330

    Update database_pg.py

--- database_pg.py
+++ database_pg.py
@@ -1,1 +1,19 @@
+import os
+import psycopg
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def get_connection():
+
+    return psycopg.connect(
+        DATABASE_URL
+    )
+  if __name__ == "__main__":
+
+    conn = get_connection()
+
+    print("✅ PostgreSQL Connected")
+
+    conn.close()
