94d38570b63ea8b9b443c3afd010b16b9968c8eb
commander000004 <commander04.1391t@gmail.com>
2026-07-20 19:04:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 from bale import Bot
-from config import TOKEN
 
+from config import TOKEN
 from database import init_db
 from router import handle_message
 
-# اول دیتابیس ساخته بشه
+
 init_db()
 
-from database_pg import get_connection
 
-try:
-    conn = get_connection()
-    print("✅ PostgreSQL Connected")
-    conn.close()
-
-except Exception as e:
-    print("❌ PostgreSQL Error:", e)
-    
-
-bot = Bot(token=TOKEN)
+bot = Bot(
+    token=TOKEN
+)
 
 
 @bot.event
 async def on_ready():
-    print("Empire Bot Started 🚀")
+
+    print(
+        "Empire Bot Started 🚀"
+    )
 
 
 @bot.event
 async def on_message(message):
+
     await handle_message(
+
         message,
+
         bot
+
     )
 
 
